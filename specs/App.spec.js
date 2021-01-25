f875a4458068cb8db1b26d40aae1d8f90078223c
Thomas <thomas@craftacademy.se>
2021-01-26 00:31:50 +0100

    adds examples of mocking state and testing components using RNTL

--- specs/App.spec.js
+++ specs/App.spec.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import { render } from '@testing-library/react-native';
 
 import App from '../App';
+import initialState from '../src/state/store/initialState'
 
 describe('<App />', () => {
-  it('has 1 child', () => {
-    const screen = render(<App />)
-    expect(screen.getByText('TuneShare').children).toHaveLength(1)
+  const mockStore = configureStore([]);
+  const store = mockStore(initialState);
+  
+  it('displays app title', () => {
+    const screen = render(<Provider store={store}><App /></Provider>)
+    // console.log(screen.debug())
+    expect(screen.getByText('TuneShare')).toBeTruthy()
+    // console.table(screen.getByText('TuneShare').props)
+    expect(screen.getByText('TuneShare').props.style.fontSize).toEqual(17)
   });
 });
--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
+import { useSelector } from 'react-redux'
 
 const HomeScreen = () => {
+  const { posts } = useSelector(state => state)
+
   return (
     <View>
       <Text>hello</Text>
+      <FlatList
+        testID="list"
+        data={posts}
+        renderItem={({ item }) => <Text>{item.track_name}</Text>}
+        keyExtractor={item => item.track_name}
+      />
     </View>
   )
 }
